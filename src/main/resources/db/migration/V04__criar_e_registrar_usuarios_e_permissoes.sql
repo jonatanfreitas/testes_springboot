2f99ad5b8c05a8628dc80e0f61b212a9cd4d1b92
Jonatan <jonatanfreitasc@gmail.com>
2022-08-03 23:01:38 -0300

    ajuste flyway codigos

--- src/main/resources/db/migration/V04__criar_e_registrar_usuarios_e_permissoes.sql
+++ src/main/resources/db/migration/V04__criar_e_registrar_usuarios_e_permissoes.sql
@@ -1,16 +1,16 @@
-CREATE TABLE usuario (
+CREATE TABLE usuario(
 	codigo BIGINT(20) PRIMARY KEY,
 	nome VARCHAR(50) NOT NULL,
 	email VARCHAR(50) NOT NULL,
 	senha VARCHAR(150) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE permissao (
+CREATE TABLE permissao(
 	codigo BIGINT(20) PRIMARY KEY,
 	descricao VARCHAR(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE usuario_permissao (
+CREATE TABLE usuario_permissao(
 	codigo_usuario BIGINT(20) NOT NULL,
 	codigo_permissao BIGINT(20) NOT NULL,
 	PRIMARY KEY (codigo_usuario, codigo_permissao),
